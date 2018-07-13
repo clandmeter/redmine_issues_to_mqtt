09458ca370463d9e05c19d804fa21cf52e5ddcd1
Carlo Landmeter <clandmeter@alpinelinux.org>
2018-07-13 08:48:06 +0000

    bump version to 0.0.2

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_issues_to_mqtt do
   name 'Issues to MQTT'
   author 'Carlo Landmeter'
   description 'A plugin to send issues metadata to MQTT'
-  version '0.0.1'
+  version '0.0.2'
   settings :partial => 'settings/issues_to_mqtt', :default => { 
     'mqtt_host' => 'test.mosquitto.org',
     'mqtt_topic' => 'issues'
